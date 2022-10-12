b0b11b893286175cfeae4e364d3aa444c1f2ced9
Joyce <joycesantos8900@gmail.com>
2022-10-12 15:27:03 -0300

    quicksort

---
+++ quicksort/quick_sort.py
@@ -0,0 +1,69 @@
+from time import time
+import random
+
+
+fim =0
+inicial = 0
+
+
+def quickSort(v,s,e):
+    if (s<e):
+        p = partition (v,s,e)
+        quickSort (v,s,p-1)
+        quickSort(v,p+1,e)
+
+    
+    
+def partition (v,s,e):
+    k =v[e]
+    j = s-1
+    auxiliar = 0
+    for i in range (s,e):
+        if v[1]<= v[i]:
+            j+=1
+            auxiliar = v[i]
+            v[j] =v[i]
+            v[i] = auxiliar
+        
+    auxiliar = v[e]
+    v[e] = v [i+1]
+    v[i+1]= auxiliar
+    return i+1
+
+
+def tw ():
+    for t in range (1,1001,100):
+        total = 0 
+        for j in range (0,10000):
+            v = []
+            v = array(v,t)
+            inicial = (time())
+            quickSort(v,0,t-1)
+            fim = (time())
+            total = total + fim-inicial
+        media = total/1000
+        print (f" {media:.5f}")
+           
+
+
+
+
+def tb ():
+    for t in range (0,1001,100):
+        total = 0
+        for j in range(0,10000):
+            v=[]
+            v=array(v,t)
+            inicial = (time())
+            quickSort(v,0,t-1)
+            fim = (time())
+            total+= fim-inicial
+        media = total/1000
+        print (f"{t}    {media:.6f}")
+
+def array (v,t):
+    for i in range (0,t):
+        v.append(i)
+    return v
+
+tw ()
